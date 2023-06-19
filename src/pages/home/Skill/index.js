1119cbab1678934b0a9cb6900e1abcbe8b835546
YoungSeungJang <1345620@naver.com>
2023-06-19 23:10:50 +0900

    :sparkles:Feat: 컨텐츠 채우기

--- src/pages/home/Skill/index.js
+++ src/pages/home/Skill/index.js
@@ -5,25 +5,30 @@ const Skill = () => {
 	return (
 		<S.Wrapper id="3">
 			<div>Skill</div>
-
-			<img
-				src="https://i.namu.wiki/i/GFgxq3pF-Jm-R5HzfAazD10byqZcPfso7XcQJTZDBxrMi_IwO1xgB3zH7d86IC0_-kAsI7AweS-ZRbJPutd30TWMqshppm4_AUmPvGJ70FV5zfMhwioPBX-3ZDHBo5d66dc_PY4FOV4BAP3woF3gPw.svg"
-				width={100}
-			/>
-
-			<img
-				src="https://i.namu.wiki/i/S854VYKF1uSlSeRE52JYbppMwaVm7yU3Ev-hsaRbR6Un6foxVfZevwBRyWJli73ZotqunOEmajvhJTJcOlu7TBTxfRMGK7-4mTg8T_re-UOa6ESYjD4RtfaISwWFq4I_0H3ZExej1uee2sOa_8ZZMQ.svg"
-				width={100}
-			/>
-
-			<img
-				src="https://i.namu.wiki/i/2WNY6CmWKE2Ygyj6YmUJNSs0I-fcf0gmDz-fc4byWRqjMyyC9SOHd0PWI2dI61RMyV6t3UbVlydTkkj7Ea_z81C0SH16waMzlLNE2j79ulS9kTurl5H8Gah7Mj7qGHgWKnpIjeqEmvWltUv6SDKScg.svg"
-				width={70}
-			/>
-			<img
-				src="https://i.namu.wiki/i/hIvF8sd9cGZGkHWB0HZg9S-yvN7Kcy4V_fNWF4ygZLKQb3ilN72OE2c_9SxFIrU4s9gkN51cKr0nEyDvb6pgf_Aro8pO_ew8l-XYRdsaiSCqTfHcXGQZuVB-dzFn4ldgV3ROVRlixJZX9Swd-Ga6EQ.svg"
-				width={160}
-			/>
+			<S.ImgBox>
+				<img
+					src="https://i.namu.wiki/i/GFgxq3pF-Jm-R5HzfAazD10byqZcPfso7XcQJTZDBxrMi_IwO1xgB3zH7d86IC0_-kAsI7AweS-ZRbJPutd30TWMqshppm4_AUmPvGJ70FV5zfMhwioPBX-3ZDHBo5d66dc_PY4FOV4BAP3woF3gPw.svg"
+					width={100}
+				/>
+			</S.ImgBox>
+			<S.ImgBox>
+				<img
+					src="https://i.namu.wiki/i/S854VYKF1uSlSeRE52JYbppMwaVm7yU3Ev-hsaRbR6Un6foxVfZevwBRyWJli73ZotqunOEmajvhJTJcOlu7TBTxfRMGK7-4mTg8T_re-UOa6ESYjD4RtfaISwWFq4I_0H3ZExej1uee2sOa_8ZZMQ.svg"
+					width={100}
+				/>
+			</S.ImgBox>
+			<S.ImgBox>
+				<img
+					src="https://i.namu.wiki/i/2WNY6CmWKE2Ygyj6YmUJNSs0I-fcf0gmDz-fc4byWRqjMyyC9SOHd0PWI2dI61RMyV6t3UbVlydTkkj7Ea_z81C0SH16waMzlLNE2j79ulS9kTurl5H8Gah7Mj7qGHgWKnpIjeqEmvWltUv6SDKScg.svg"
+					width={70}
+				/>
+			</S.ImgBox>
+			<S.ImgBox>
+				<img
+					src="https://i.namu.wiki/i/hIvF8sd9cGZGkHWB0HZg9S-yvN7Kcy4V_fNWF4ygZLKQb3ilN72OE2c_9SxFIrU4s9gkN51cKr0nEyDvb6pgf_Aro8pO_ew8l-XYRdsaiSCqTfHcXGQZuVB-dzFn4ldgV3ROVRlixJZX9Swd-Ga6EQ.svg"
+					width={160}
+				/>
+			</S.ImgBox>
 		</S.Wrapper>
 	);
 };
@@ -37,8 +42,12 @@ const Wrapper = styled.div`
 `;
 
 const ImgBox = styled.div`
-	width: 100px;
-	height: 100px;
+	width: 150px;
+	height: 150px;
+	background-color: #f8e8ee;
+	${flexAllCenter}
+	margin: 20px;
+	border-radius: 20px;
 `;
 const S = {
 	Wrapper,
